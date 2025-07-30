34be3f4d6a147ac64fcc6ebb2da8a262005864ed
Raphaël Pietrzak <raphael.pietrzak@ynov.com>
2025-07-30 21:08:43 +0200

    Correction de la configuration CORS pour permettre l'accès à l'API depuis localhost en plus de l'URL de production.

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use('/api/stripe/webhook', express.raw({ type: 'application/json' }));
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://uniforms-cnd.vercel.app",
+  origin: "https://uniforms-cnd.vercel.app" || "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
